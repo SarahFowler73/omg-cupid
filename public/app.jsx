1fc29520f6ae01511fb88abc5c77d4b7486f62b3
Sarah Fowler <sarahf321@hotmail.com>
2017-03-19 18:45:50 -0400

    Subdivide into components; probably should have an input component; don't know how to have a generalized component yet

--- public/app.jsx
+++ public/app.jsx
@@ -26,49 +26,86 @@ Header.propTypes = {
     author: React.PropTypes.string.isRequired
 }
 
+function UserNameInput(){
+    return (
+        <div className="form-group">
+            <label htmlFor="username">Username:</label>
+            <input id="username" type="text"/>
+        </div>
+    );
+}
+
+function AgeInput(props){
+    return (
+        <div className="form-group">
+            <label htmlFor="age">Age: </label>
+            <select id="age">
+              {props.ageOpts.map(function(age) {
+                  return <option>{age}</option>
+              })}
+            </select>
+        </div>
+    );
+}
+
+AgeInput.propTypes = {
+    ageOpts: React.PropTypes.array.isRequired
+}
+
+function SexInput() {
+    /* todo: radio button component? */
+    return (
+        <div className="form-group">
+            <label htmlFor="sex">Sex:</label>
+            <div className="radio">
+
+              <label>
+                  <input type="radio" name="sex" value="male"/>
+                  Male
+              </label>
+              <label>
+                  <input type="radio" name="sex" value="female"/>
+                  Female
+              </label>
+              <label>
+                  <input type="radio" name="sex" value="terrible"/>
+                  Yes, please! // pop up 'don't be one of those'
+              </label>
+            </div>
+        </div>
+    );
+}
+
+function DescriptionInput() {
+    return (
+        <div className="form-group">
+          <label htmlFor="description">Describe yourself in 3 syllables: </label>
+          <input type="text" id="description" value="Maybe check a dictionary to see how many syllables the thing is"/>
+        </div>
+    );
+}
+
+function LookingFor() {
+    return (
+        <div className="form-group">
+          <label>Looking for: </label>
+          <div></div>
+        </div>
+    );
+}
+
 function ProfileForm(props) {
     return (
         <form className="profileForm">
           <div className="form-group">
             <h2>Make Your Profile!</h2>
           </div>
-          <div className="form-group">
-              <label htmlFor="username">Username:</label>
-              <input id="username" type="text"/>
-          </div>
-          <div className="form-group">
-              <label htmlFor="age">Age: </label>
-              <select id="age">
-                {props.ageOpts.map(function(age) {
-                    return <option>{age}</option>
-                })}
-              </select>
-          </div>
-          <div className="form-group">
-              <label htmlFor="sex">Sex:</label>
-              <div className="radio">
-                <label>
-                    <input type="radio" name="sex" value="male"/>
-                    Male
-                </label>
-                <label>
-                    <input type="radio" name="sex" value="female"/>
-                    Female
-                </label>
-                <label>
-                    <input type="radio" name="sex" value="terrible"/>
-                    Yes, please! // pop up 'don't be one of those'
-                </label>
-              </div>
-          </div>
-          <div className="form-group">
-            <label htmlFor="description">Describe yourself in 3 syllables: </label>
-            <input type="text" id="description" value="Maybe check a dictionary to see how many syllables the thing is"/>
-          </div>
-          <div className="form-group">
-            <label>Looking for: </label>
-            <div></div>
-          </div>
+          <UserNameInput />
+          <AgeInput ageOpts={props.ageOpts} />
+          <SexInput />
+          <DescriptionInput />
+          <LookingFor />
+
         </form>
     );
 }
