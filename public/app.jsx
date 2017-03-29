b87c4bb8006718aef3d8ceca32fb8b75d279b80f
Sarah Fowler <sarahf321@hotmail.com>
2017-03-28 22:17:36 -0400

    moving local form state to form, dammit

--- public/app.jsx
+++ public/app.jsx
@@ -54,73 +54,12 @@ RadioButton.propTypes = {
     })
 }
 
-function ProfileForm(props) {
-    return (
-        <form className="profileForm w3-card-2 w3-round w3-white">
-          {/* Form title */}
-          <div className="form-group">
-            <h2>Make Your Profile!</h2>
-          </div>
-          {/* Username */}
-          <div className="form-group">
-              <label htmlFor="username">Username:</label>
-              <input id="username" type="text" onBlur={function(evt){props.setValue(evt, 'username')}}/>
-          </div>
-          {/* Age group selection */}
-          <div className="form-group">
-              <label htmlFor="age">Age: </label>
-              <select id="age" onChange={function(evt){props.setValue(evt, 'ageChoice')}}>
-                {props.ageOpts.map(function(age, i) {
-                    return <option key={i}>{age}</option>
-                })}
-              </select>
-          </div>
-          {/* Sex selection */}
-          <div className="form-group">
-              <label htmlFor="sex">Sex:</label>
-              <div className="radio">
-                {props.sexOpts.map(function(button){
-                  return <RadioButton key={button.id} button={button} />
-                })}
-              </div>
-          </div>
-          {/* Description */}
-          <div className="form-group">
-            <label htmlFor="description">Describe yourself in 3 syllables: </label>
-            <input type="text" id="description" onBlur={function(evt){props.checkDescription(evt)}}/>
-          </div>
-          {/* Looking For Checkboxes */}
-          <div className="form-group">
-            <label>Looking for: </label>
-            {props.lookingFor.map(function(box, i){
-                return <label key={i}>{box}: <input type="checkbox" name="looking-for" value={i} onChange={function(evt){props.setChoices(evt, 'lookingFor')}} /></label>
-            })}
-          </div>
-          <button type='submit' className="w3-margin">Make My Profile!</button>
-        </form>
-    );
-}
 
-ProfileForm.PropTypes = {
-    ageOpts: React.PropTypes.array.isRequired,
-    sexOpts: React.PropTypes.arrayOf(
-        React.PropTypes.shape({
-            id: React.PropTypes.number.isRequired,
-            name: React.PropTypes.string.isRequired,
-            value: React.PropTypes.string.isRequired,
-            label: React.PropTypes.string.isRequired
-        })
-    ).isRequired,
-    lookingFor: React.PropTypes.array.isRequired,
-    setValue: React.PropTypes.func.isRequired,
-    setChoices: React.PropTypes.func.isRequired,
-    checkDescription: React.PropTypes.func.isRequired
-}
 
-let Application = React.createClass({
+let ProfileForm = React.createClass({
 
     propTypes: {
-        ageOpts: React.PropTypes.array,
+        ageOpts: React.PropTypes.array.isRequired,
         sexOpts: React.PropTypes.arrayOf(
             React.PropTypes.shape({
                 id: React.PropTypes.number.isRequired,
@@ -128,15 +67,11 @@ let Application = React.createClass({
                 value: React.PropTypes.string.isRequired,
                 label: React.PropTypes.string.isRequired
             })
-        ),
-        lookingFor: React.PropTypes.array,
-        userProfile: React.PropTypes.shape({
-            username: React.PropTypes.string.isRequired,
-            ageChoice: React.PropTypes.string.isRequired,
-            sexChoice: React.PropTypes.string.isRequired,
-            description: React.PropTypes.string.isRequired,
-            lookingFor: React.PropTypes.arrayOf(React.PropTypes.string).isRequired,
-        })
+        ).isRequired,
+        lookingFor: React.PropTypes.array.isRequired,
+        setValue: React.PropTypes.func.isRequired,
+        setChoices: React.PropTypes.func.isRequired,
+        checkDescription: React.PropTypes.func.isRequired
     },
 
     getDefaultProps: function(){
@@ -149,41 +84,93 @@ let Application = React.createClass({
 
     getInitialState: function() {
         return {
-            userProfile: {
-                username: "",
-                ageChoice: "",
-                sexChoice: "",
-                description: "",
-                lookingFor: [],
-            }
+            username: "",
+            age: 0,
+            sex: 0,
+            description: "",
+            lookingFor: [],
+            canCount: false,
+            warning: {"username": false}
         }
+    },
+
+    validateUsername: function(evt, value){
+        if (evt.target.value.match(/\s/)) {
+            this.state.warning[value] = true;
+        } else {
+            this.state.warning[value] = false;
+            this.state[value] = evt.target.value;
+        }
+        this.setState(this.state);
     },
 
     render: function() {
         return (
-          <div>
-              <Header />
-              <ProfileForm
-                  ageOpts={this.props.ageOpts}
-                  sexOpts={this.props.sexOpts}
-                  lookingFor={this.props.lookingFor}
-                  userProfile={this.state.userProfile}
-                  setValue={this.setValue}
-                  setChoices={this.setChoices}
-                  checkDescription={this.checkDescription}
-              />
-          </div>
+            <div id="profile-form" className="w3-card-2 w3-round w3-white w3-center w3-margin w3-padding">
+                <h3>Make Your Profile!</h3>
+                <form className="profileForm" onSubmit="">
+                  {/* Username */}
+                  <div className="form-group">
+                      <label htmlFor="username">Username:</label>
+                      <input
+                          id="username"
+                          type="text"
+                          value={this.state.username}
+                          onChange={function(evt){this.validateUsername(evt, 'username')}.bind(this)}
+                      />
+                      <div className="warning" display={this.state.warning.username ? "block" : "none"}>NO SPACES!</div>
+                  </div>
+                  {/* Age group selection */}
+                  <div className="form-group">
+                      <label htmlFor="age">Age: </label>
+                      <select id="age" onChange={function(evt){this.setValue(evt, 'age')}}>
+                        {this.props.ageOpts.map(function(age, i) {
+                            return <option key={i} value={i}>{age}</option>
+                        })}
+                      </select>
+                  </div>
+                  {/* Sex selection */}
+                  <div className="form-group">
+                      <label htmlFor="sex">Sex:</label>
+                      <div className="radio" onChange={function(evt){this.setValue(evt, 'sex')}}>
+                        {this.props.sexOpts.map(function(button){
+                          return <RadioButton key={button.id} button={button} />
+                        })}
+                      </div>
+                  </div>
+                  {/* Description */}
+                  <div className="form-group">
+                    <label htmlFor="description">Describe yourself in 3 syllables: </label>
+                    <input type="text" id="description" onBlur={function(evt){this.checkDescription(evt)}}/>
+                  </div>
+                  {/* Looking For Checkboxes */}
+                  <div className="form-group">
+                    <label>Looking for: </label>
+                    {this.props.lookingFor.map(function(box, i){
+                        return <label key={i}>{box}: <input type="checkbox" name="lookingFor" value={i} onChange={function(evt){this.setChoices(evt, 'lookingFor')}} /></label>
+                    })}
+                  </div>
+                  <input type='submit' className="w3-margin" value="Make My Profile!"/>
+                </form>
+            </div>
         );
     },
 
-    /* Application methods */
     setValue: function(event, field) {
-        this.state.userProfile[field] = event.target.value;
+        this.state[field] = event.target.value;
         this.setState(this.state);
     },
 
-    setChoices: function(choices, field) {
-        this.state.userProfile[field] = choices.length > 1 ? choices : choices[0];
+    setChoices: function(choice, field) {
+        // todo: this is fucking awful!
+        if (choice.target.checked) {
+            this.state[field].push(choice.target.value);
+        }else {
+            this.state[field].splice(
+                this.state[field].indexOf(choice.target.value),
+                1
+            );
+        }
         this.setState(this.state);
     },
 
@@ -193,9 +180,51 @@ let Application = React.createClass({
         if (numSyls == 3) {
             alert('Nice job! We\'ll add "can count" to your profile!');
             this.setValue(event, 'description');
+
         } else
             alert("Uh. That's not three syllables.");
+    },
+    validateProfile: function() {
+
     }
+})
+
+
+let Application = React.createClass({
+
+    propTypes: {
+        userProfile: React.PropTypes.shape({
+            username: React.PropTypes.string.isRequired,
+            ageChoice: React.PropTypes.string.isRequired,
+            sexChoice: React.PropTypes.string.isRequired,
+            description: React.PropTypes.string.isRequired,
+            lookingFor: React.PropTypes.arrayOf(React.PropTypes.string).isRequired,
+        })
+    },
+
+    getInitialState: function() {
+        return {
+            userProfile: {
+                username: null,
+                ageChoice: null,
+                sexChoice: null,
+                description: null,
+                lookingFor: [],
+                canCount: null
+            }
+        }
+    },
+
+    render: function() {
+        return (
+          <div>
+              <Header />
+              <ProfileForm />
+          </div>
+        );
+    },
+
+    /* Application methods */
 
 })
 
