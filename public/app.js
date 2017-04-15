e407443200aac11e1d4c87c31ae82a88f478bc5b
Sarah Fowler <sarahf321@hotmail.com>
2017-04-15 16:37:00 -0400

    Convert to using bundled dependencies

--- public/app.js
+++ public/app.js
@@ -1,55 +1,59 @@
-const AGE_CHOICES = ["Pick one:", "18-24", "25-29", "30-34", "35+"]
+// Libs
+var React = require('react');
+var ReactDOM = require('react-dom');
 
-const SEX_CHOICES = ['Male', 'Female', 'Other']
+const AGE_CHOICES = ['Pick one:', '18-24', '25-29', '30-34', '35+'];
 
-const LOOKING_FOR = ["Long Term", "Short Term", "One Night", "One Minute"]
+const SEX_CHOICES = ['Male', 'Female', 'Other'];
+
+const LOOKING_FOR = ['Long Term', 'Short Term', 'One Night', 'One Minute'];
 
 function randomElem(arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
+  return arr[Math.floor(Math.random() * arr.length)];
 }
 
 function Header(props) {
-    return (
-    <div className="w3-padding w3-block w3-theme-l1 w3-left-align">
+  return (
+  <div className='w3-padding w3-block w3-theme-l1 w3-left-align'>
       <h1>Welcome to OmgCupid!</h1>
       <p><em>A simulated dating experience!</em></p>
     </div>
-    );
+  );
 }
 
 function RadioButton(props) {
-    return (
-        <label>
-            <input type="radio" name={props.name} value={props.value} checked={props.checked}/>
-            {" " + props.label}
+  return (
+      <label>
+            <input type='radio' name={props.name} value={props.value} checked={props.checked}/>
+            {' ' + props.label}
         </label>
-    );
+  );
 }
 
 RadioButton.propTypes = {
     name: React.PropTypes.string.isRequired,
     value: React.PropTypes.string.isRequired,
     label: React.PropTypes.string.isRequired,
-    checked: React.PropTypes.string.isRequired
-}
+    checked: React.PropTypes.string.isRequired,
+  };
 
 function InputField(props) {
-    return (
-        <div className="form-group">
+  return (
+      <div className='form-group'>
             <label htmlFor={props.name}>{props.label}:</label>
-            <div className="popup">
+            <div className='popup'>
                 <input
-                    id={props.name}
-                    type="text"
-                    value={props.value}
-                    onChange={function(evt){props.validateInputText(evt, props.name)}}
+                  id={props.name}
+                  type='text'
+                  value={props.value}
+                  onChange={function (evt) { props.validateInputText(evt, props.name); }}
                 />
-                <span className="popuptext" style={{display: props.display}}>
+                <span className='popuptext' style={{display: props.display}}>
                     {props.warning}
                 </span>
             </div>
         </div>
-    );
+  );
 }
 
 InputField.propTypes = {
@@ -59,80 +63,87 @@ InputField.propTypes = {
     validateInputText: React.PropTypes.func.isRequired,
     display: React.PropTypes.string.isRequired,
     warning: React.PropTypes.string.isRequired,
-}
+  };
 
 function AgeField(props) {
-    return (
-        <div className="form-group">
-            <label htmlFor="age">Age: </label>
-            <div className="popup">
-                <select id="age" onChange={function(evt){props.validateAgeChoice(evt, 'age')}}>
-                    {props.ageOpts.map(function(age, i) {
-                        return i === 0 ?
-                             <option key={i} value={age} selected disabled>{age}</option>
-                            :
-                             <option key={i} value={age}>{age}</option>
+  return (
+      <div className='form-group'>
+            <label htmlFor='age'>Age: </label>
+            <div className='popup'>
+                <select id='age'
+                        onChange={function (evt) { props.validateAgeChoice(evt, 'age'); }}
+                >
+                    {props.ageOpts.map(function (age, i) {
+                      return i === 0 
+                         ?
+                           <option key={i} value={age} selected disabled>{age}</option>
+                          :
+                           <option key={i} value={age}>{age}</option>;
 
                     })}
 
                 </select>
-                <span className="popuptext" style={{display: props.display}}>{props.warning}</span>
+                <span className='popuptext' style={{display: props.display}}>
+                    {props.warning}
+                </span>
             </div>
         </div>
-    );
+  );
 }
 
 function SexField(props) {
-    return (
-        <div className="form-group">
-            <label htmlFor="sex">Sex:</label>
+  return (
+      <div className='form-group'>
+            <label htmlFor='sex'>Sex:</label>
 
-            <div className="radio popup" onChange={function(evt){props.validateSexChoice(evt, 'sex')}}>
-              {props.sexOpts.map(function(opt, i){
-                return <RadioButton
-                    key={i}
-                    name='sex'
-                    label={opt}
-                    value={opt.toLowerCase()}
-                    checked={props.whichChecked == opt.toLowerCase() ? "checked" : ""}/>
+            <div className='radio popup'
+                 onChange={function (evt) {props.validateSexChoice(evt, 'sex');}}
+            >
+              {props.sexOpts.map(function (opt, i) {
+              return <RadioButton
+                  key={i}
+                  name='sex'
+                  label={opt}
+                  value={opt.toLowerCase()}
+                  checked={props.whichChecked == opt.toLowerCase() ? 'checked' : ''}/>;
             })}
-              <span className="popuptext" style={{display: props.display}}>{props.warning}</span>
+              <span className='popuptext' style={{display: props.display}}>{props.warning}</span>
             </div>
         </div>
-    );
+  );
 }
 
 SexField.propTypes = {
     sexOpts: React.PropTypes.arrayOf(React.PropTypes.string).isRequired,
     validateSexChoice: React.PropTypes.func.isRequired,
     display: React.PropTypes.string.isRequired,
-    whichChecked: React.PropTypes.string.isRequired
-}
+    whichChecked: React.PropTypes.string.isRequired,
+  };
 
 function LookingFor(props) {
-    return (
-        <div className="form-group">
+  return (
+      <div className='form-group'>
         <label>Looking for: </label>
-        {props.lookingFor.map(function(box, i){
-            return <label key={i}>{box}:
+        {props.lookingFor.map(function (box, i) {
+          return <label key={i}>{box}:
                 <input
-                    type="checkbox"
-                    name="lookingFor"
-                    value={i}
-                    onChange={function(evt){props.validateLookingFor(evt)}}
-                    checked={props.whichChecked.indexOf(String(i)) > -1 ? 'checked' : ''}
-                />
-            </label>
+                  type='checkbox'
+                  name='lookingFor'
+                  value={i}
+                  onChange={function (evt) {props.validateLookingFor(evt);}}
+                  checked={props.whichChecked.indexOf(String(i)) > -1 ? 'checked' : ''}
+              />
+            </label>;
         })}
         </div>
-    );
+  );
 }
 
 LookingFor.propTypes = {
     lookingFor: React.PropTypes.array.isRequired,
     validateLookingFor: React.PropTypes.func.isRequired,
     whichChecked: React.PropTypes.array.isRequired
-}
+  };
 
 let ProfileForm = React.createClass({
 
@@ -141,77 +152,79 @@ let ProfileForm = React.createClass({
         sexOpts: React.PropTypes.arrayOf(React.PropTypes.string).isRequired,
         lookingFor: React.PropTypes.array.isRequired,
         submitForm: React.PropTypes.func.isRequired
-    },
+      },
 
-    getDefaultProps: function(){
+    getDefaultProps: function () {
         return {
             ageOpts: AGE_CHOICES,
             sexOpts: SEX_CHOICES,
-            lookingFor: LOOKING_FOR
-        }
-    },
+            lookingFor: LOOKING_FOR,
+          };
+      },
 
-    getInitialState: function() {
+    getInitialState: function () {
         return {
-            username: "",
-            age: "",
-            sex: "",
-            description: "",
+            username: '',
+            age: '',
+            sex: '',
+            description: '',
             lookingFor: [],
             canCount: false,
-            warnings: []
-        }
-    },
+            warnings: [],
+          };
+      },
 
-    existsInArray: function(value, stateArrayKey) {
-        return this.state[stateArrayKey].filter(function (val) {return val == value}).length;
-    },
+    existsInArray: function (value, stateArrayKey) {
+        return this.state[stateArrayKey].filter(
+            function (val) {return val == value;}
+        ).length;
+      },
 
-    addToArray: function(value, stateArrayKey) {
+    addToArray: function (value, stateArrayKey) {
         let arr = this.state[stateArrayKey].slice();
         if (!this.existsInArray(value, stateArrayKey))
             arr.push(value);
         return arr;
-    },
+      },
 
-    removeFromArray: function(value, stateArrayKey) {
+    removeFromArray: function (value, stateArrayKey) {
         let arr = this.state[stateArrayKey].slice();
         if (this.existsInArray(value, stateArrayKey))
             arr.splice(arr.indexOf(value), 1);
         return arr;
-    },
+      },
 
-    validateInputText: function(evt, value){
+    validateInputText: function (evt, value) {
         this.state[value] = evt.target.value; /* set the value */
 
         /* Handle warnings for spaces */
-        this.state.warnings = /\s/.test(evt.target.value)
-            ?
+        this.state.warnings = /\s/.test(evt.target.value) ?
             this.addToArray(value, 'warnings')
             :
             this.removeFromArray(value, 'warnings');
         this.setState(this.state);
-    },
+      },
 
-    validateAgeSexChoice: function(evt, component) {
-        let toCheck = component === 'sex' ? 'other' : '35+'
+    validateAgeSexChoice: function (evt, component) {
+        let toCheck = component === 'sex' ? 'other' : '35+';
         if (evt.target.value === toCheck) {
-            this.state.warnings = this.addToArray(component, 'warnings');
-            this.state[component] = "";
+          this.state.warnings = this.addToArray(component, 'warnings');
+          this.state[component] = '';
         } else {
-            this.state.warnings = this.removeFromArray(component, 'warnings');
-            this.state[component] = evt.target.value;
+          this.state.warnings = this.removeFromArray(component, 'warnings');
+          this.state[component] = evt.target.value;
         }
-        this.setState(this.state);
-    },
 
-    validateLookingFor: function(evt) {
+        this.setState(this.state);
+      },
+
+    validateLookingFor: function (evt) {
         this.state.lookingFor = evt.target.checked ?
             this.addToArray(evt.target.value, 'lookingFor')
             :
             this.removeFromArray(evt.target.value, 'lookingFor');
         this.setState(this.state);
-    },
+      },
 
     checkDescription: function (description) {
 
@@ -221,62 +234,66 @@ let ProfileForm = React.createClass({
 
                 this.state.canCount = responseData.length && responseData[0].numSyllables === 3;
                 this.setState(this.state);
-            }.bind(this))
-    },
+              }.bind(this));
+      },
 
-    checkMissing: function() {
+    checkMissing: function () {
         // Return true if not all values are filled out
         // minus warnings and canCount
         return Object.values(this.state).filter(
-            function (val){return val && val.length}
+            function (val) {return val && val.length;}
         ).length < Object.values(this.state).length - 2;
-    },
+      },
 
-    validateProfile: function(evt, props) {
+    validateProfile: function (evt, props) {
         evt.preventDefault();
         if (this.state.warnings.length || this.checkMissing()) {
-            alert('You have invalid or empty values in your profile!');
+          alert('You have invalid or empty values in your profile!');
         } else {
-            this.checkDescription(this.state.description);
-            this.props.submitForm(this.state);
+          this.checkDescription(this.state.description);
+          this.props.submitForm(this.state);
         }
 
-    },
+      },
 
-    render: function() {
+    render: function () {
         return (
-            <div id="profile-form" className="w3-card-2 w3-round w3-white w3-center w3-margin w3-padding">
+            <div id='profile-form'
+                 className='w3-card-2 w3-round w3-white w3-center w3-margin w3-padding'
+            >
                 <h3>Make Your Profile!</h3>
-                <form className="profileForm">
+                <form className='profileForm'>
                     <InputField
-                        name="username"
-                        label="Username"
+                        name='username'
+                        label='Username'
                         value={this.state.username}
                         validateInputText={this.validateInputText}
-                        display={this.existsInArray('username', 'warnings') ? "block" : "none"}
-                        warning="No spaces in usernames!"
+                        display={this.existsInArray('username', 'warnings') ? 'block' : 'none'}
+                        warning='No spaces in usernames!'
                      />
                     <AgeField
                         ageOpts={this.props.ageOpts}
                         validateAgeChoice={this.validateAgeSexChoice}
-                        display={this.existsInArray('age', 'warnings') ? "block" : "none"}
-                        warning={"Ew. No one wants to think about old people dating. Go away."}
+                        display={this.existsInArray('age', 'warnings') ? 'block' : 'none'}
+                        warning={'Ew. No one wants to think about old people dating. Go away.'}
                     />
                     <SexField
                         sexOpts={this.props.sexOpts}
                         validateSexChoice={this.validateAgeSexChoice}
-                        display={this.existsInArray('sex', 'warnings') ? "block" : "none"}
+                        display={this.existsInArray('sex', 'warnings') ? 'block' : 'none'}
                         whichChecked={this.state.sex}
-                        warning={"Unfortunately, the only joke I have depends on binary sex choice. Sorry. :("}
+                        warning={'Unfortunately, the only joke I have ' +
+                            'depends on binary sex choice. Sorry. :('
+                        }
                     />
 
                     <InputField
-                        name="description"
-                        label="Describe yourself using one three-syllable word"
+                        name='description'
+                        label='Describe yourself using one three-syllable word'
                         value={this.state.description}
                         validateInputText={this.validateInputText}
-                        display={this.existsInArray('description', 'warnings') ? "block" : "none"}
-                        warning="Just one word!"
+                        display={this.existsInArray('description', 'warnings') ? 'block' : 'none'}
+                        warning='Just one word!'
                     />
                     <LookingFor
                         lookingFor={this.props.lookingFor}
@@ -285,18 +302,16 @@ let ProfileForm = React.createClass({
                     />
                     <input
                         type='submit'
-                        className="w3-margin"
-                        value="Make My Profile!"
+                        className='w3-margin'
+                        value='Make My Profile!'
                         onClick={this.validateProfile}
                     />
                 </form>
             </div>
         );
-    },
+      },
 
-
-})
-
+  });
 
 let Application = React.createClass({
 
@@ -307,10 +322,10 @@ let Application = React.createClass({
             sexChoice: React.PropTypes.string.isRequired,
             description: React.PropTypes.string.isRequired,
             lookingFor: React.PropTypes.arrayOf(React.PropTypes.string).isRequired,
-        })
-    },
+          }),
+      },
 
-    getInitialState: function() {
+    getInitialState: function () {
         return {
             userProfile: {
                 username: null,
@@ -318,34 +333,35 @@ let Application = React.createClass({
                 sexChoice: null,
                 description: null,
                 lookingFor: [],
-                canCount: null
-            }
-        }
-    },
+                canCount: null,
+              },
+          };
+      },
 
-    render: function() {
+    render: function () {
         return (
           <div>
               <Header />
               <ProfileForm submitForm={this.makeProfile}/>
           </div>
         );
-    },
+      },
 
     /* Application methods */
 
-    makeProfile: function(profileObj) {
+    makeProfile: function (profileObj) {
         this.state.userProfile.username = profileObj.username;
         this.state.userProfile.ageChoice = profileObj.age;
         this.state.userProfile.sexChoice = profileObj.sex;
         this.state.userProfile.description = profileObj.description;
-        this.state.userProfile.lookingFor = profileObj.lookingFor.map(idx => LOOKING_FOR[parseInt(idx)]) ;
+        this.state.userProfile.lookingFor = profileObj.lookingFor.map(
+            idx => LOOKING_FOR[parseInt(idx)]
+        );
         this.state.userProfile.canCount = profileObj.canCount;
 
         this.setState(this.state);
-    }
+      },
 
-})
-
+  });
 
 ReactDOM.render(<Application/>, document.getElementById('container'));
