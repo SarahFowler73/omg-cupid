fcea7708bf0c59117bf391ed46e127148f7f2004
Sarah Fowler <sarahf321@hotmail.com>
2017-05-28 13:52:08 -0400

    Add mailbox routing

--- public/components/mailbox.js
+++ public/components/mailbox.js
@@ -1,30 +1,44 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
 
 var users = ['hey', 'ho'];
 
 export function Mailbox(props) {
     return (
         <div>
-            <ul>
-                <label>Sent</label>
-                {
-                    props.gender === 'male'
-                    ?
-                    props.messages.map( (message, i) => <li><span>To: {users[i]}</span> { message }</li>)
-                    :
-                    <li>Nothing here!</li>
+            <Link to='/mail/outbox'>Sent</Link>
+            <Link to='/mail/inbox'>Received</Link>
+            <Route
+                path='/mail/outbox'
+                render={ () =>
+                    <ul>
+                        <label>Sent</label>
+                        {
+                            props.gender === 'male'
+                            ?
+                            props.messages.map( (message, i) => <li key={i}><span>To: {users[i]}</span> { message }</li>)
+                            :
+                            <li>Nothing here!</li>
+                        }
+                    </ul>
                 }
-            </ul>
-            <ul>
-                <label>Received</label>
-                {
-                    props.gender === 'female'
-                    ?
-                    props.messages.map( (message, i) => <li><span>To: {users[i]}</span> { message }</li>)
-                    :
-                    <li>Nothing here!</li>
+            />
+            <Route
+                path='/mail/inbox'
+                render={ () =>
+                    <ul>
+                        <label>Received</label>
+                        {
+                            props.gender === 'female'
+                            ?
+                            props.messages.map( (message, i) => <li key={i}><span>To: {users[i]}</span> { message }</li>)
+                            :
+                            <li>Nothing here!</li>
+                        }
+                    </ul>
                 }
-            </ul>
+            />
+
         </div>
     );
 }
