1622987f44a985f5a2a36cb5794b087990f539b8
Sarah Fowler <sarahf321@hotmail.com>
2017-07-24 10:48:35 -0400

    Better logo; more styling. Ugh, so much styling

--- public/components/header.js
+++ public/components/header.js
@@ -6,8 +6,10 @@ export function Header(props) {
       <div className='w3-top'>
           <div className='w3-padding w3-block w3-theme-l1 w3-left'>
               <div id='logo' className='w3-left'>
-                  <h1 className='w3-bar-item'><img src='/assets/omg-logo.png'/></h1>
+                  <h1 className='w3-bar-item w3-left'><img src='/assets/omg-logo2.png'/>
                   <p className='w3-bar-item w3-center'><em>A simulated dating experience!</em></p>
+                  </h1>
+
               </div>
               <nav className='w3-right-align'>
               { props.has_profile
